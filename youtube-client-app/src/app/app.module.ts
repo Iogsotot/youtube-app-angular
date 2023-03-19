376f19b4a3d960a933c8ba8a169a21852ba38671
Anna Iustus <ajustusa@gmail.com>
2023-03-19 14:50:58 +0100

    feat: improve basic project structure

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import AppRoutingModule from './app-routing.module';
-import AppComponent from './app.component';
+import AppRoutingModule from "./app-routing.module";
+import AppComponent from "./app.component";
+import HeaderComponent from "./core/header/header.component";
+import SearchComponent from "./shared/search/search.component";
+import FooterComponent from "./core/footer/footer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-
-export default class AppModule { }
+export default class AppModule {}
