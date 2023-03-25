635b137de9a858e4256f3d1e643924a75a0b789c
Anna Iustus <ajustusa@gmail.com>
2023-03-25 21:00:20 +0100

    feat: add basic structure (skeleton)

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,19 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 import { AppComponent } from '.';
 import { HeaderComponent, FooterComponent } from './core';
 import { SearchComponent } from './shared/search';
+import { CardComponent } from './shared/card/card.component';
+import { FilterComponent } from './feature/filter/filter.component';
+import { SearchResultComponent } from './feature/search-result/search-result.component';
+import { LogoComponent } from './shared/logo/logo.component';
+import { LoginComponent } from './feature/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    HeaderComponent, 
-    SearchComponent, 
-    FooterComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    FooterComponent,
+    CardComponent,
+    FilterComponent,
+    SearchResultComponent,
+    LogoComponent,
+    LoginComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
