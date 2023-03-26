74bae24a0b05d9b22d486ecdb80c0c405833a60e
Anna Iustus <ajustusa@gmail.com>
2023-03-26 21:33:08 +0200

    feat: add colored band for card (by date)

--- youtube-client-app/src/app/feature/filter/filter.component.ts
+++ youtube-client-app/src/app/feature/filter/filter.component.ts
@@ -3,9 +3,21 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent {
+  // By clicking on the Settings button, the Filtering criteria block should be toggled
+  // It should be possible to sort search results by date or count of views
+  // Sorting should work both in the direction of decreasing values and in the direction of increasing values
+  // Using a pipe, filter search results by value that a user types in the input
+
+  sortValue = 'tag?';
+
+  onWordSort(e: Event) {
+    // менять response.items по нужной сортировке
+    console.log(this.sortValue, e);
+  }
+
   @Input()
   isOpen: boolean = false;
 }
--- youtube-client-app/src/app/shared/card/card.component.ts
+++ youtube-client-app/src/app/shared/card/card.component.ts
@@ -1,34 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { YoutubeResponseItemModel } from 'src/app/models/youtube.model';
+
+const enum CardColorEnum {
+  BLUE = 'blue', // 7d
+  GREEN = 'green', // 7d - 1 month
+  YELLOW = 'yellow', // 1m - 6m
+  RED = 'red', // 6m+
+}
+
+const getColorByDate = (inputDate: string | Date): CardColorEnum => {
+  const date = new Date(inputDate);
+  const currentDate = new Date();
+
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const day = date.getDay();
+
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+  const currentDay = currentDate.getDay();
+
+  if (currentYear - year > 1) {
+    return CardColorEnum.RED;
+  }
+  if (currentMonth - month > 6 || (currentYear - year === 1 && currentMonth - month <= 6)) {
+    return CardColorEnum.YELLOW;
+  }
+  if (currentMonth - month === 1 || (currentMonth - month === 0 && currentDay - day > 7)) {
+    return CardColorEnum.GREEN;
+  }
+  return CardColorEnum.BLUE;
+};
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {
-
-  // enum for color
-  // If older than 6 months – Red
-// If from a month up to 6 months – Yellow
-// If from seven days up to a month – Green
-// If younger than 7 days – Blue
-  color = 'blue'
+export class CardComponent implements OnInit {
+  color: CardColorEnum = CardColorEnum.BLUE;
 
   @Input()
-  card!: YoutubeResponseItemModel | null;
+  card!: YoutubeResponseItemModel;
 
-
-  // почему не работает? card undefined
-  // thumbnail = this.card.snippet.thumbnails;
-
-  // viewsCount = this.card.statistics.viewCount;
-
-  // likesCount = this.card.statistics.likeCount;
-
-  // dislikesCount = this.card.statistics.dislikeCount;
-
-  // commentsCount = this.card.statistics.commentCount;
-
-  // title = this.card.snippet.title;
+  ngOnInit() {
+    this.color =
+      this.card && this.card.snippet.publishedAt
+        ? getColorByDate(this.card.snippet.publishedAt)
+        : CardColorEnum.BLUE;
+  }
 }
